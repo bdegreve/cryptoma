d87b4e8a337c1ee1590287cb881569b1bc1694d3
Bram de Greve <bram.degreve@bramz.net>
2016-12-10 19:25:48 +0100

    Fixing warning about pullRight

--- app/components/navbar/index.js
+++ app/components/navbar/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router'
 
-import Nav from 'react-bootstrap/lib/Nav'
 import Navbar from 'react-bootstrap/lib/Navbar'
 
 export default ({brand, children}) => {
-  const childs = React.Children.toArray(children)
-  const lefts = childs.filter((child) => !child.props.pullRight)
-  const rights = childs.filter((child) => child.props.pullRight)
-
   return (
     <Navbar inverse fixedTop>
       <Navbar.Header>
@@ -18,12 +13,7 @@ export default ({brand, children}) => {
         <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
-        <Nav>
-          {lefts}
-        </Nav>
-        <Nav pullRight>
-          {rights}
-        </Nav>
+        {children}
       </Navbar.Collapse>
     </Navbar>
   )
--- app/pages/layout/index.js
+++ app/pages/layout/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router'
 
+import Nav from 'react-bootstrap/lib/Nav'
 import NavItem from 'react-bootstrap/lib/NavItem'
 import NavDropdown from 'react-bootstrap/lib/NavDropdown'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
@@ -12,17 +13,21 @@ import style from './style.less'
 export default ({children}) => (
   <div>
     <AppBar brand='Static-React-Bootstrap'>
-      <NavLink to='/crypto'>Crypto</NavLink>
-      <NavItem eventKey={2}>Action</NavItem>
-      <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-      </NavDropdown>
-      <NavItem pullRight eventKey={1}>Action Right</NavItem>
-      <NavLink pullRight to='/about'>About</NavLink>
+      <Nav>
+        <NavLink to='/crypto'>Crypto</NavLink>
+        <NavItem eventKey={2}>Action</NavItem>
+        <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1}>Action Right</NavItem>
+        <NavLink to='/about'>About</NavLink>
+      </Nav>
     </AppBar>
     <div className={style.main}>
       {children}
