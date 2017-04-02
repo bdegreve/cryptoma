6e0b74a4f8fddbe7693d65de54e2bf489ea8abbd
Bram de Greve <bram.degreve@bramz.net>
2017-04-02 13:21:11 +0200

    adding numberstation

--- app/ciphers/index.js
+++ app/ciphers/index.js
@@ -1,4 +1,5 @@
 import doppelkastenschlussel from './doppelkastenschlussel'
+import numberstation from './number-station'
 import pancake from './pancake'
 import opolopoTaal from './opolopo-taal'
 import reverse from './reverse'
@@ -7,6 +8,7 @@ import vigenere from './vigenere'
 
 export default {
   doppelkastenschlussel,
+  numberstation,
   pancake,
   opolopoTaal,
   reverse,
