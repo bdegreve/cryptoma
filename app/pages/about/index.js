5cd1c124e36e35c06354ccccf33a696b08e65af9
Bram de Greve <bram.degreve@bramz.net>
2017-04-16 11:12:53 +0200

    About text

--- app/pages/about/index.js
+++ app/pages/about/index.js
@@ -6,9 +6,11 @@ import Grid from 'react-bootstrap/lib/Grid'
 export default () => (
   <Grid componentClass='main'>
     <header>
-      <h1>About</h1>
+      <h1>About Cryptoma</h1>
     </header>
-    <p>things</p>
+    <p>This tool was created for the purpose of enciphering parts of the route
+      description of <a href='http://www.joepie27.be/'>Joepie 27</a>, tocht C.</p>
+    <p>Full source code is available on <a href='https://github.com/bdegreve/cryptoma'>Github</a>.</p>
     <footer>
       <Link to='/'>Home</Link>
     </footer>
