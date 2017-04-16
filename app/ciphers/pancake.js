f4a7c94d1f7800c56b89399a689a6a73739fec0e
Bram de Greve <bram.degreve@bramz.net>
2017-04-16 19:04:08 +0200

    Pancake: better random numbers

--- app/ciphers/pancake.js
+++ app/ciphers/pancake.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Button from 'react-bootstrap/lib/Button'
 
 import TextBox from 'components/text-box'
 
 import reverse from 'lib/reverse'
 import withoutWhitespace from 'lib/without-whitespace'
-import shuffle from 'lib/shuffle'
+import { randomFloats } from 'lib/random'
 
 export default {
   name: 'Pancake',
@@ -21,23 +22,46 @@ export default {
       <TextBox
         label='Key numbers'
         placeholder='Enter numbers here, seperated with commas'
-        value={value}
+        value={value || ''}
         onChange={(v) => { onChange(v.replace(/[^0-9]+/g, ',')) }}
         controlId='pancake-key'
       />
-      <button type='button' onClick={() => {
-        const n = withoutWhitespace(plaintext).length
-        const numbers = [n]
-        for (let k = 0; k < n; ++k) {
-          const x = Math.ceil(Math.pow(Math.random(), 1.5) * (n - 1)) + 1
-          numbers.push(x)
-        }
-        onChange(shuffle(numbers).join(','))
+      <Button onClick={() => {
+        const numbers = randomKeyNumbers(plaintext)
+        onChange(numbers.join(','))
       }}
       >
-        Random
-      </button>
+        Generate random keynumbers
+      </Button>
     </div>
+}
+
+function randomKeyNumbers (plaintext) {
+  const n = withoutWhitespace(plaintext).length
+
+  // generate n integers in range [2, n]
+  let numbers = []
+  const rng = randomFloats()
+  while (numbers.length < n) {
+    const x = rng.next().value
+    const k = 2 + Math.floor(Math.pow(x, 2) * (n - 1))
+    if (numbers.length > 0 && numbers[numbers.length - 1] === k) {
+      continue // avoid duplicates
+    }
+    numbers.push(k)
+  }
+
+  // insert at least one <n> somewhere in the middle
+  while (true) {
+    const i = Math.floor((rng.next().value + 0.5) * (n / 2))
+    if (numbers[i] === n || numbers[i + 1] === n) {
+      continue // avoid duplicates
+    }
+    numbers.splice(i, 0, n)
+    break
+  }
+
+  return numbers
 }
 
 const sort = (text, numbers) =>
---
+++ app/lib/random.js
@@ -0,0 +1,50 @@
+const crypto = typeof window !== 'undefined'
+  ? window.crypto || window.msCrypto || undefined
+  : undefined
+
+const N = 4294967296 // 2 ** 32
+
+export const hasCrypto = !!crypto
+
+// generates random numbers in range [0, 1), 1 not included.
+export function * randomFloats () {
+  if (crypto) {
+    let xs = new Uint32Array(16)
+    let k = xs.length
+    while (true) {
+      if (k === xs.length) {
+        crypto.getRandomValues(xs)
+        k = 0
+      }
+      yield xs[k++] / N
+    }
+  } else {
+    while (true) {
+      yield Math.random()
+    }
+  }
+}
+
+// generates random integers in range [0, n), n not included
+export function * randomIntegers (n) {
+  if (crypto) {
+    const Nprime = Math.floor(N / n) * n
+    let xs = new Uint32Array(16)
+    let k = xs.length
+    while (true) {
+      if (k === xs.length) {
+        crypto.getRandomValues(xs)
+        k = 0
+      }
+      const x = xs[k++]
+      if (x >= Nprime) {
+        continue
+      }
+      yield x % n
+    }
+  } else {
+    while (true) {
+      yield Math.floor(n * Math.random())
+    }
+  }
+}
